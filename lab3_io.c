7a0e811663578ad0b28eda0098c274141def18f9
dvynjli <dvynjli@gmail.com>
2019-04-01 23:24:05 +0530

    initial commit

---
+++ lab3_io.c
@@ -0,0 +1,28 @@
+#include "lab3_io.h"
+
+void read_matrix (const char* input_filename, int* M, int* N, double** D){
+	FILE *fin = fopen(input_filename, "r");
+
+	fscanf(fin, "%d%d", M, N);
+	
+	int num_elements = (*M) * (*N);
+	*D = (double*) malloc(sizeof(double)*(num_elements));
+	
+	for (int i = 0; i < num_elements; i++){
+		fscanf(fin, "%lf", (*D + i));
+	}
+	fclose(fin);
+}
+
+void write_result (int M, 
+		int N, 
+		double* D, 
+		double* U, 
+		double* SIGMA, 
+		double* V_T,
+		int K, 
+		double* D_HAT,
+		double computation_time){
+	// Will contain output code
+
+}
